87f8fcf66c6b1f4c0c744c0aa90dc5ec44d81600
John Rexter Flores <johnrexter.flores@gmail.com>
2020-11-08 20:05:19 +0800

    Create order table model and migration

---
+++ database/migrations/2020_11_08_120053_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->double('sub_total');
+            $table->double('total');
+            $table->double('quantity');
+            $table->bigInteger('payment_id')->nullable();
+            $table->enum('status', ['pending', 'processing', 'accepted', 'for-delivery', 'delivered', 'cancelled', 'on-hold'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
