d70bfb807595addf6be7728b414c182f875746b2
John Rexter Flores <johnrexter.flores@gmail.com>
2020-11-08 14:08:49 +0800

    Remove unused function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,31 +28,4 @@ class HomeController extends Controller
 
         return view('home', compact('user'));
     }
-
-    public function profile(CreateReportRequest $request)
-    {
-        $user = auth()->user();
-
-        // $validatedData = $request->validate([
-        //     'first_name' => 'required|min:2',
-        //     'last_name' => 'required|min:2',
-        //     'email' => 'required|email',
-        //     'phone' => 'required',
-        //     'company' => 'nullable',
-        //     'position' => 'nullable',
-        //     'username' => 'required|min:4',
-        // ]);
-
-        $user->update([
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'company' => $request->company,
-            'position' => $request->position,
-            'username' => $request->username
-        ]);
-
-        return view('dashboard.profile', compact('user'));
-    }
 }
