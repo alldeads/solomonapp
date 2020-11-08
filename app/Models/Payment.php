46263299253b09a5b330ba8a2bc6a5eef651ea71
John Rexter Flores <johnrexter.flores@gmail.com>
2020-11-08 20:12:10 +0800

    Create payment model and migration

---
+++ app/Models/Payment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'address_id',
+        'reference_code',
+        'amount',
+        'status'
+    ];
+
+    public function order()
+    {
+    	return $this->belongsTo(Order::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+}
