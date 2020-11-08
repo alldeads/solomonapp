c8f620a81feee73d33e8a89cf42ad3e6a8b4b95f
John Rexter Flores <johnrexter.flores@gmail.com>
2020-11-09 01:28:31 +0800

    Create items model and migration

---
+++ database/migrations/2020_11_08_165510_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('quantity')->default(0);
+            $table->double('points');
+            $table->string('avatar')->nullable();
+            $table->integer('redeemed')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
