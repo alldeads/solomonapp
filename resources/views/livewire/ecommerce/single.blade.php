345a98d39bb0599df7303b085c5032ca51ddbc43
John Rexter Flores <johnrexter.flores@gmail.com>
2020-12-17 14:21:32 +0800

    Update htaccess file

--- resources/views/livewire/ecommerce/single.blade.php
+++ resources/views/livewire/ecommerce/single.blade.php
@@ -7,7 +7,7 @@
 		@endif
       	<div class="product-box">
          	<div class="product-img">
-            	<img class="img-fluid" src="{{ str_replace('public', 'storage', asset($product->avatar)) }}" alt="">
+            	<img class="img-fluid" src="{{ asset($product->avatar) }}" alt="">
             	<div class="product-hover">
                		<ul>
                   		<li>
