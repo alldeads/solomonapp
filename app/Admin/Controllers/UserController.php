7ea75c7f608e8383dde02844ca9cc03838303420
John Rexter Flores <johnrexter.flores@gmail.com>
2020-12-20 02:35:48 +0800

    Remove company and position in edit admin

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -96,8 +96,6 @@ class UserController extends AdminController
         $form->text('username', __('Username'));
         $form->email('email', __('Email'));
         $form->text('phone', __('Phone'));
-        $form->text('company', __('Company'));
-        $form->text('position', __('Position'));
         $form->number('available_points', __('Available points'));
 
         $form->select('status', __('Status'))
