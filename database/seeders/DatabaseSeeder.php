fbab851c67012f217676f43f02405e9239a161de
John Rexter Flores <johnrexter.flores@gmail.com>
2021-01-25 01:41:42 +0800

    Add solomon direct account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -47,30 +47,27 @@ class DatabaseSeeder extends Seeder
             'zip'  => ''
         ]);
 
-    	// for ($i=1; $i < 10; $i++) { 
-    	// 	\App\Models\User::factory(2)->create([
-    	// 		'sponsor_id' => $i,
-    	// 		'available_points' => 0,
-     //            'status' => 'active'
-    	// 	]);
+        $user = User::create([
+            'first_name' => "SolomonDirectAccount",
+            'last_name'  => "Account",
+            'email' => 'contact@solomonapp.com',
+            'username' => 'solomondirectaccount',
+            'phone' => '123456',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 
+            'status' => 'active'
+        ]);
 
-    	// 	$user = User::find($i);
-
-    	// 	$user->direct_recruits = 2;
-    	// 	$user->save();
-
-     //        Address::create([
-     //            'user_id' => $user->id,
-     //            'first_name' => $user->first_name,
-     //            'last_name' => $user->last_name,
-     //            'email' => $user->email,
-     //            'phone' => $user->phone,
-     //            'address'  => '',
-     //            'state'  => '',
-     //            'city'  => '',
-     //            'zip'  => ''
-     //        ]);
-    	// }
+        Address::create([
+            'user_id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'phone' => $user->phone,
+            'address'  => '',
+            'state'  => '',
+            'city'  => '',
+            'zip'  => ''
+        ]);
 
         PaymentMethod::create([
             'name' => 'Cash On Delivery',
