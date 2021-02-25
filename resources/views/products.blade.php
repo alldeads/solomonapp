3d10995f1410c4206c9926471be498577693171f
John Rexter Flores <johnrexter.flores@gmail.com>
2021-02-26 03:21:57 +0800

    Refactor site pages

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-	
-	<div class="row justify-content-center">
-		<div class="col-12 m-auto">
-            <div class="owl-carousel nav-inside show-nav-hover dots-inside mb-0" data-plugin-options="{'items': 1, 'loop': true, 'autoplay': true, 'autoplayTimeout': 3000, 'autoplayHoverPause': true, 'nav': true, 'dots': true, 'animateOut': 'fadeOut'}">
-
-                @for( $i = 2; $i < 3; $i++ )
-                    <div>
-                        <img src="{{ asset('sliders/'. $i .'.png') }}" data-thumb="{{ asset('sliders/'. $i .'.png') }}" alt="" />
-                    </div>
-                @endfor
-            </div>
-        </div>
-	</div>
+    
+    @livewire('partials.slider-container', ['from' => 2, 'to' => 3])
 
 @endsection
